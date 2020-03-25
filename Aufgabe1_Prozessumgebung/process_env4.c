baf218b79bd4a0bc6a6316874ec698865e52ee94
JoProt <47474907+JoProt@users.noreply.github.com>
2020-03-25 13:44:28 +0100

    update process_env4.

--- Aufgabe1_Prozessumgebung/process_env4.c
+++ Aufgabe1_Prozessumgebung/process_env4.c
@@ -1,16 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 int main(int argc, char *argv[], char *envp[])
 {
         /*functions to manipulate environment: getenv(); putenv(); setenv(); unsetenv(); clearenv()*/
 
 
         /*getenv()*/
-        printf("getenv(HOME): %s \n", getenv("HOME"));
+        printf("getenv(HOME): %s\n", getenv("HOME"));
 
-        /*the other functions cant be called, because it's not allowed in posix standart and ANSI-C*/
+        /*putenv()*/
+        putenv("env1=ich habe hallo gesetzt");
+        printf("putenv(Hallo): %s\n", getenv("env1"));
+        /*setenv()*/
+        setenv("home", ":~/Echtzeit_und_Netzwerk_Programmierung/process_env$",1);
+        printf("setenv(home,:~/Echtzeit_und_Netzwerk_Programmierung/process_env$,1): %s \n ",getenv("home"));
+
+        /*unsetenv()*/
+        unsetenv("home");
+        printf("unsetenv(home) -> home = %s\n",getenv("home"));
+
+        /*clearenv()*/
+        clearenv();
+           printf("clearenv()am Beispiel getenv(HOME) = %s \n",getenv("HOME"));
+           printf("clearenv()am Beispiel getenv(LANG) = %s \n",getenv("LANG"));
+           printf("clearenv()am Beispiel getenv(PWD) = %s \n",getenv("PWD"));
+
 
 
 
         return EXIT_SUCCESS;
+}
