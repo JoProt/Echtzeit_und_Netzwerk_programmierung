c149da61384ed0c7f1fd42bdbabe21a6c34c4ef8
JoProt <47474907+JoProt@users.noreply.github.com>
2020-03-30 19:24:57 +0200

    update exec.c

--- Aufgabe_1 ProcessExec/process_exec.c
+++ Aufgabe_1 ProcessExec/process_exec.c
@@ -2,25 +2,29 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
+
 
 int main (int argc, char * argv[]){
-        char *argumente[] = {argv[1], NULL};
+
+        char *argumente[argc];
+
+
+        for (int i = 0; i <= argc; i++){
+
+                argumente[i] = argv[i+1];
+                if(i == argc-1) argumente[i] = NULL;
+
+        }
 
         switch (argc){
 
         case 1:
                 printf("kein Aufrufargument\n");
                 break;
-        case 2:
-                printf("argument = %s \n", argumente[0]);
+        default:
                 printf("execute ...\n");
                 execvp(argumente[0], argumente);
                 break;
-        default:
-                printf("nur 1 Aufrufargument\n");
-                break;
-
         }
 
         return EXIT_SUCCESS;
