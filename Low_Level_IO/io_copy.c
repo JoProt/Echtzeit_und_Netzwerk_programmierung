09fe09a2e4a67e03783f2871aa7d19c51ac04116
JoProt <47474907+JoProt@users.noreply.github.com>
2020-05-03 19:15:49 +0200

    update io

--- Low_Level_IO/io_copy.c
+++ Low_Level_IO/io_copy.c
@@ -1,64 +1,61 @@
-#include <stdlib.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv){
-
-        if(argc != 2)printf("Aufruf: ./copy + buffer size");
-        else{
-        ssize_t ret_in, ret_out;
-        int buffer = *argv[1];
-        int fd_oeffnen;
-        int fd_kopiert;
-        char* name[20];
-        char* charBuf[buffer];
-        printf("Dateiname, welche kopiert werden soll: ");
-
-        scanf("%s", &name);
-
-        if((fd_oeffnen = open(name,O_CREAT| O_TRUNC, O_RDONLY))==-1){
-                  printf("Oeffnen der zu kopierenden Datei fehlgeschlagen\n");
-                  return (EXIT_FAILURE);
-         }
-        //verbinden von strings
-
-        char* s = "Kopierte-";
-        char * connectednames;
-
-        connectednames = malloc(strlen(name)+ strlen(s));
-
-        strcpy(connectednames, s);
-        strcat(connectednames,name);
-
-        //---------------
-
-        if((fd_kopiert = open(connectednames, O_CREAT | O_WRONLY))==-1){
-           printf("Oeffnen einer neuen Datei zum kopieren fehlgeschlagen\n");
-           return (EXIT_FAILURE);
-         }
-        else{
-                while((ret_in = read (fd_oeffnen, &charBuf, buffer)) > 0){
-                       ret_out = write(fd_kopiert, &charBuf, (ssize_t) ret_in);
-                         if(ret_out != ret_in){  //Write error
-                             printf("write error\n");
-                             return (EXIT_FAILURE);
-                         }
-                }
-                printf("... kopiert.\n");
-
+int main(int argc, char *argv[], char *envp[])
+{
+        if(argc != 3){
+                printf("Bitte Dateinamen und Buffersize wie folgt angeben!\n");
+                printf("./io_copy dateiname buffersize\n");
+                return (EXIT_FAILURE);
         }
 
+        else {
+                 ssize_t in, out;
+                  int output, input;
+
+                  //verbinden von strings
+
+                char* s = "Kopierte-";
+                char * connectednames;
+
+                connectednames = malloc(strlen(argv[1])+ strlen(s));
+
+                strcpy(connectednames, s);
+                strcat(connectednames,argv[1]);
+
+   //---------------
+
+                //buffer zweiter parameter in argv
+          int buffer = *argv[2];
+          char buf[buffer];     // array of char mit groesse des buffers (argv[2])
+
+          //Datei oeffnen
+          if((input = open(argv[1], O_RDONLY))==-1){
+                printf("Oeffnen der zu kopierenden Datei fehlgeschlagen\n");
+                return (EXIT_FAILURE);
+          }
+          if((output = open(connectednames, O_CREAT | O_WRONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH))==-1){
+                printf("Oeffnen einer neuen Datei zum kopieren fehlgeschlagen\n");
+                return (EXIT_FAILURE);
+          }
 
 
+          //kopieren
+          while((in = read(input, &buf, buffer)) > 0){
+                out = write(output, &buf, (ssize_t)in);
+                        if(out != in){
+                                printf("Datei konnte nicht vollstaendig kopiert werden\n");
+                                return (EXIT_FAILURE);
+                        }
+          }
+          printf("... Datei wurde kopiert.\n");
 
-
-        close(fd_oeffnen);
-        close(fd_kopiert);
-        return EXIT_SUCCESS;
+          close(input);
+          close(output);
+        }
+  return (EXIT_SUCCESS);
 }
-~
-~
-~
--- Low_Level_IO/io_ls.c
+++ Low_Level_IO/io_ls.c
@@ -1,17 +1,31 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
-int main()
+int main(int argc, char** argv)
 {
+        if(argc != 2){
+                printf("Usage: Uebergabeparameter ist der Filename \n");
+                return EXIT_FAILURE;
+        }
+        else{
         struct stat st;
-        char* name[20];
-        printf("name:");
-        scanf("%s", &name);
-        stat(name, &st);
-        printf("filesize (byte): %ld\nblocksize (byte): %ld\nblocks: %ld\n", st.st_size, st.st_blksize, st.st_blocks);
+        char *name = argv[1];
+        int fd;
+
+        if( fd = open(argv[1], O_WRONLY)!=-1) {
+                        stat(name, &st);
+                        printf("filesize (byte): %ld\nblocksize (byte): %ld\nblocks: %ld\n", st.st_size, st.st_blksize, st.st_blocks);
+                }
+        else {
+                        printf("Datei nicht gefunden\n");
+                        return (EXIT_FAILURE);
+                }
 
 
         return (EXIT_SUCCESS);
+        }
 }
-
